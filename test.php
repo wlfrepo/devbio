677daa9d3899959d4f84cff8bf2f29a6ab843fb8
minno <minno@WIN-VMN79EKIITF>
2016-12-10 12:33:36 +0100

    change ip to 127.0.0.1

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$address = '127.0.0.2';
+$address = '127.0.0.1';
 $port = 9000;
 $sock = socket_create(AF_INET, SOCK_STREAM, 0);
 socket_bind($sock, $address, $port) or die('Unable to bind');
